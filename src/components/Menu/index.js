d93ac5e3dcaf121db70e74318dbf3f8799aaf71b
Ralisson-Mattias <ralissonlp@gmail.com>
2020-07-28 17:26:26 -0300

    Upload 01

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
+import { Link } from 'react-router-dom';
 import './Menu.css';
 import Button from '../Button';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Mutaflix logo" />
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo video
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+const Pagina404 = () => (<div>Pagina 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
